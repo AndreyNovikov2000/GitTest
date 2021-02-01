4da4afdb1a6b0617548e922357c5659b6f3b15c2
Novikov Andrey <novikovandrey2000@gmail.com>
2021-02-01 21:08:50 +0600

    Create Network Layer

---
+++ GitTest/NetworlLayer.swift
@@ -0,0 +1,14 @@
+//
+//  NetworlLayer.swift
+//  GitTest
+//
+//  Created by Andrey Novikov on 2/1/21.
+//
+
+import Foundation
+
+class NetworkLayer {
+    static let shared = NetworkLayer()
+    
+    private init() { }
+}
---
+++ GitTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Andrey Novikov on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Hello")
+
+    }
+
+
+}
+
